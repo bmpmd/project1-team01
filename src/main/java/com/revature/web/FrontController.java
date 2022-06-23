61fb8d3606eccd68d28799079b2b537f0b10d892
gaschindler <gaschindler2018@gmail.com>
2022-06-23 17:03:49 -0400

    implmented servlet/service layer functionality for employee logins

---
+++ src/main/java/com/revature/web/FrontController.java
@@ -0,0 +1,34 @@
+package com.revature.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// rewrite the URI to capture the relevant information
+		// http://localhost:8080/project1-team01/login -- capture "login"
+		final String URI = request.getRequestURI().replace("/project1-team01/", "");
+		
+		// set up a switch case statement to call the
+		// appropriate functionality based on the URI parsed above
+		switch (URI) {
+		case "login":
+			RequestDispatcher.processLogin(request, response);
+			
+			break;
+		default:
+			// TODO: send to a custom error page
+			break;
+		}
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+}
