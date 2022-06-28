fbe12b43c42ec078e4165bc3f8ee4e4746893cbe
teeny909 <sk8leo@live.com>
2022-06-27 20:30:37 -0400

    manager page complete except for employees button

---
+++ src/main/webapp/homepage.js
@@ -0,0 +1,8 @@
+
+let emp = sessionStorage.getItem('user');
+console.log("employee below: ");
+console.log(emp);
+
+let jsonstr = sessionStorage.getItem('jsonstr');
+console.log("json below: ");
+console.log(jsonstr);
