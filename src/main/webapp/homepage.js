4b4c0b68356f0c2d36a229c6ed73c3eee24ff06a
teeny909 <sk8leo@live.com>
2022-06-28 19:15:35 -0400

    javascript ready for fetching json for tables

--- src/main/webapp/homepage.js
+++
@@ -1,8 +0,0 @@
-
-let emp = sessionStorage.getItem('user');
-console.log("employee below: ");
-console.log(emp);
-
-let jsonstr = sessionStorage.getItem('jsonstr');
-console.log("json below: ");
-console.log(jsonstr);
